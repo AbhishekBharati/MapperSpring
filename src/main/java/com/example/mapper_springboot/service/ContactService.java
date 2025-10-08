5b34fa2f3adaf67a771225c0da74a328f25d47f0
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-10-08 16:11:35 +0530

    Implemented contactMapper instead of manually writing everything

--- src/main/java/com/example/mapper_springboot/service/ContactService.java
+++ src/main/java/com/example/mapper_springboot/service/ContactService.java
@@ -2,6 +2,7 @@ package com.example.mapper_springboot.service;
 
 import com.example.mapper_springboot.dto.ContactDTO;
 import com.example.mapper_springboot.entity.Contact;
+import com.example.mapper_springboot.mapper.ContactMapper;
 import com.example.mapper_springboot.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,34 +17,40 @@ public class ContactService {
     @Autowired
     private ContactRepository contactRepository;
 
+    @Autowired
+    private ContactMapper contactMapper;
+
     public Contact saveContact(ContactDTO contactDTO){
-        Contact contact = Contact.builder()
-                .firstName(contactDTO.getFirstName())
-                .lastName(contactDTO.getLastName())
-                .email(contactDTO.getEmail())
-                .phoneNo(contactDTO.getPhoneNo())
-                .build();
-        return contactRepository.save(contact);
+//        Contact contact = Contact.builder()
+//                .firstName(contactDTO.getFirstName())
+//                .lastName(contactDTO.getLastName())
+//                .email(contactDTO.getEmail())
+//                .phoneNo(contactDTO.getPhoneNo())
+//                .build();
+//        return contactRepository.save(contact);
+        return contactRepository.save(contactMapper.toEntity(contactDTO));
     }
 
     public ContactDTO getContactById(Long id){
-        Optional<Contact> contact = contactRepository.findById(id);
-        return contact.map(value -> ContactDTO.builder()
-                .firstName(value.getFirstName())
-                .lastName(value.getLastName())
-                .email(value.getEmail())
-                .phoneNo(value.getPhoneNo())
-                .build()).orElse(null);
+//        Optional<Contact> contact = contactRepository.findById(id);
+//        return contact.map(value -> ContactDTO.builder()
+//                .firstName(value.getFirstName())
+//                .lastName(value.getLastName())
+//                .email(value.getEmail())
+//                .phoneNo(value.getPhoneNo())
+//                .build()).orElse(null);
+        return contactMapper.toDTO(contactRepository.findById(id).orElse(new Contact()));
     }
 
     public List<ContactDTO> getContactList(){
-        List<Contact> contacts = contactRepository.findAll();
-        return contacts.stream().map(contact ->
-                ContactDTO.builder()
-                        .firstName(contact.getFirstName())
-                        .lastName(contact.getLastName())
-                        .email(contact.getEmail())
-                        .phoneNo(contact.getPhoneNo())
-                        .build()).toList();
+//        List<Contact> contacts = contactRepository.findAll();
+//        return contacts.stream().map(contact ->
+//                ContactDTO.builder()
+//                        .firstName(contact.getFirstName())
+//                        .lastName(contact.getLastName())
+//                        .email(contact.getEmail())
+//                        .phoneNo(contact.getPhoneNo())
+//                        .build()).toList();
+        return contactMapper.toDTOList(contactRepository.findAll());
     }
 }
